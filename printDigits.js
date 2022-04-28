43849428a54522e31556f89a4276ebcdc6494659
Esteban Pecina <estebanpecina14@gmail.com>
2022-04-28 15:09:01 -0500

    Initial commit

---
+++ printDigits.js
@@ -0,0 +1,9 @@
+const printDigits = (num) => {
+    while(num) {
+        let digitToPrint = num % 10;
+        console.log(digitToPrint)
+        num = (num - digitToPrint) / 10;
+    }
+}
+
+printDigits(12)
